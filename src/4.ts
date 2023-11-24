56be341337858628350db4f4d0fad3614c48cb0c
Valerii Viun <2455521@gmail.com>
2023-11-24 20:58:05 +0200

    Update 4.ts

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,59 @@
+
+class Key {
+    private signature: number;
+
+    constructor() {
+      this.signature = Math.random();
+    }
+
+    getSignature(): number {
+      return this.signature;
+    }
+  }
+
+class Person {
+    constructor (private key: Key){}
+    getKey(): Key { 
+        return this.key;
+
+    }
+}
+
+abstract  class House  {
+  protected door: boolean = false;
+  protected key: Key;
+  protected tenants: typeof person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+
+}
+abstract openDoor(key: Key): void;
+
+comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log('Person entered the house.');
+    } else {
+      console.log('The door is closed. Cannot enter.');
+    }
+  }
+}
+  class MyHouse extends House {
+    openDoor(key: Key): void {
+      if (key.getSignature() === this.key.getSignature()) {
+        this.door = true;
+        console.log('The door is opened.');
+      } else {
+        console.log('Invalid key. The door remains closed.');
+      }
+    }
+}
+
+
+
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
